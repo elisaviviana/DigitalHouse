b838dbdb87a5496d01e2b860086fcbe7958bf37d
Elisa Staiti <eli.viviana@gmail.com>
2019-11-12 17:49:17 -0300

    martes 12/11

---
+++ practicas/java-arrays.js
@@ -0,0 +1,16 @@
+var canciones = [
+  {
+    id: 1,
+    nombre: "Manuelita",
+    puntaje: 8.5
+  },
+  {
+    id: 2,
+    nombre: "La reina batata",
+    puntaje: 7.8
+  }
+]
+
+var titulos = canciones.map(function (elemento) {
+  return elemento = canciones.nombre;
+})
